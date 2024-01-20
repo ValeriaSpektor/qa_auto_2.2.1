134e65533f39be1b9a2f58ad7d19f7ce9d7e2ae0
Valeria Spektor <vlapin84@gmail.com>
2024-01-20 12:31:04 +0200

    corrected code for hw_2.2.2

--- calculations.js
+++ calculations.js
@@ -1,26 +1,26 @@
 function increaseBalance(x, y) {
-  x = x + y;
+  x += y;
   return x;
 }
 
 function decreaseBalance(x, y) {
-x = x - y;
+x += y;
 return x;
 }
 
 function divideBalanceByAccounts(x, y) {
-  x = x / y;
+  x /= y;
   return x;
 }
 
 function getRestAfterDivision(x, y) {
-  x = x % y;
+  x %= y;
   return x;
 }
 
 console.log(increaseBalance(3000, 700));
 console.log(decreaseBalance(3000, 700));
-console.log(divideBalanceByAccounts(3000, 2, 4000));
+console.log(divideBalanceByAccounts(3000, 2));
 console.log(getRestAfterDivision(7000, 3));
 
 
